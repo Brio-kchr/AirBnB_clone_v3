5635344adb18159137ba66eb1116bd6c11f755c8
brio kchr <b.kchr4@gmail.com>
2024-05-28 16:44:19 -0700

    Alvz trial

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
-""" Flask Application """
-from flask import Flask, make_response, jsonify
+"""
+Main api file to be be run from the root directory with the command:
+ $ HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd
+    HBNB_MYSQL_HOST=127.0.0.1 HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db
+    HBNB_API_HOST=0.0.0.0 HBNB_API_PORT=5000 python3 -m api.v1.app
+"""
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
 from os import getenv
 
+
+API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+API_PORT = getenv("HBNB_API_PORT", 5000)
+
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_storage(self):
-    """that calls storage.close()"""
+def close_api(exception):
+    """Method to close storage"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_foun(error):
-    """ Loads a custom 404 page not found """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def page_not_found(e):
+    """Handler for page not found"""
+    return {"error": "Not found"}, 404
 
 
-if __name__ == "__main__":
-    """ Main Function """
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
-
-    app.run(host, int(port), threaded=True)
+if __name__ == '__main__':
+    app.run(host=API_HOST, port=API_PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""Endpoint(route) status"""
-from flask import Flask
+"""holds general api views"""
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def status():
-    """
-    Returns a JSON status
-    """
+    """Returns the state of the API server"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Retrieves the number of each objects by type:
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+@app_views.route('/stats')
+def stats():
+    """Returns a count of stored objects by type"""
+    obj_counts = {}
+    for key, cls in classes.items():
+        obj_counts[key] = storage.count(cls)
+    return jsonify(obj_counts)
