882436245d6d032d2839fa42014a282079b1b069
brio kchr <b.kchr4@gmail.com>
2024-05-28 16:18:31 -0700

    Sez check documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-"""
-Starts AirBnB Flask main application run with command:
-    $ HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd
-    HBNB_MYSQL_HOST=127.0.0.1 HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db
-    HBNB_MYSQL_HOST=127.0.0.1 HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db
-"""
-from flask import Flask, jsonify
+""" Flask Application """
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 from os import getenv
 
-API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-API_PORT = getenv('HBNB_API_PORT', 5000)
-
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_api(exception):
-    """ Closes storage if we encounter an exception """
+def close_storage(self):
+    """that calls storage.close()"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """returns a status code response"""
-    return jsonify({"error": "Not found"}), 404
+def page_not_foun(error):
+    """ Loads a custom 404 page not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host=API_HOST, port=API_PORT, threaded=True)
+    """ Main Function """
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
