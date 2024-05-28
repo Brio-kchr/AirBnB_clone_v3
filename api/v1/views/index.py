57dde69e5319c9ccae6a02e19250073fed3cb81f
brio kchr <b.kchr4@gmail.com>
2024-05-28 16:40:29 -0700

    Trial

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""
-To create all blueprints for our Flask web application
-"""
-
-from flask import Flask, jsonify
+"""Endpoint(route) status"""
+from flask import Flask
+from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
-classes = {"amenities": Amenity, "cities": City, "places": Place,
-           "reviews": Review, "states": State, "users": User}
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
-    """ Returns the status of an application """
+    """
+    Returns a JSON status
+    """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
-def stats():
-    """ Retrieves the number of eeach objects by type """
-    obj_count_dict = {}
-    for key, value in classes.items():
-        obj_count_dict[key] = storage.count(value)
-    return jsonify(obj_count_dict)
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type:
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
